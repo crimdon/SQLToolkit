19e16029df638d2921eebcb1b2b8371f963b5963
Andrew Lackenby <crimdon@hotmail.com>
2017-09-14 14:06:49 +0100

    Revert "Bug fix to TOC feature"
    This reverts commit 6ade61c1848e9fef8571623860d14bb147fdd813.
    
    # Conflicts:
    #	RunSqlScripts/RunSqlScripts.ps1
    #	RunSqlScripts/task.json
    #	vss-extension.json

--- RunSqlScripts/RunSqlScripts.ps1
+++ RunSqlScripts/RunSqlScripts.ps1
@@ -47,12 +47,15 @@ Try {
             $SqlCmd.Connection = $SqlConnection
             $SqlCmd.CommandTimeout = $queryTimeout
 
+<<<<<<< HEAD
             if ($executionOrder -eq $Env:BUILD_SOURCESDIRECTORY)
             {
                 $executionOrder = ""
             }
+=======
+            Write-Host "Running all scripts in $pathToScripts";
+>>>>>>> parent of 6ade61c... Bug fix to TOC feature
             if ([string]::IsNullOrEmpty($executionOrder)) {
-                Write-Host "Running all scripts in $pathToScripts";
                 foreach ($sqlScript in Get-ChildItem -path "$pathToScripts" -Filter *.sql | sort-object) {	
                     Write-Host "Running Script " $sqlScript.Name
 		
@@ -70,7 +73,10 @@ Try {
                 }
             }
             else {
+<<<<<<< HEAD
                 Write-Host "Using file $executionOrder"
+=======
+>>>>>>> parent of 6ade61c... Bug fix to TOC feature
                 Get-Content $executionOrder -Encoding UTF8 | ForEach-Object {
                     $sqlScript = $pathToScripts + "\" + $_
                     Write-Host "Running Script " $sqlScript
